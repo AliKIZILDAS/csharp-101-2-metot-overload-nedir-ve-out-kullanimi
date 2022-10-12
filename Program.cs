66c7cce5c47782e80025a18fa5c8f5d7fe3e2428
Ali Kızıldaş <ali.kizildas@gmail.com>
2022-10-12 08:33:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //out paremetreler
+            string sayi="999";
+
+            bool sonuc=int.TryParse(sayi,out int Outref);
+            if(sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(sonuc);
+            }
+            else
+                Console.WriteLine("Başarısız");
+                        
+            Metotlar instance=new Metotlar();
+            instance.Topla(4,5,out int toplaSonucu);
+            Console.WriteLine(toplaSonucu);
+            //Metot aşırı yükleme~Overloading
+            int ifade=999;
+            Console.WriteLine(ifade);
+
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam=a+b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+    }
+
+}
